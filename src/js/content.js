f276fce64e3055faa59311e070f4b02e524fc3e0
Logan <logan@canddi.com>
2018-08-17 10:25:14 +0100

    Fuck tabs

--- src/js/content.js
+++ src/js/content.js
@@ -12,12 +12,12 @@ var _recentBranches = '.RecentBranches';
 var $recentBranches = $(_recentBranches);
 
 if ($recentBranches && $recentBranches.length > 0) {
-	var links = $recentBranches.find('a');
+    var links = $recentBranches.find('a');
 
-	for (var a = 0; a <= links.length; a++) {
-		var $link = $(links[a]);
-		if ($link.attr('href') && $link.attr('href').indexOf('master...')) {
-			$link.attr('href', $link.attr('href').replace('master...', 'develop...'));
+    for (var a = 0; a <= links.length; a++) {
+        var $link = $(links[a]);
+        if ($link.attr('href') && $link.attr('href').indexOf('master...')) {
+            $link.attr('href', $link.attr('href').replace('master...', 'develop...'));
         }
-	}
+    }
 }
